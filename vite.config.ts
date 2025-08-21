b72b8645eadda1489b8af0863298ef891766073a
Suycode05 <suytripathi@gmail.com>
2025-08-21 10:17:57 +0530

    added new branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
