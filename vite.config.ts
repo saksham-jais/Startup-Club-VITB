014a1dbd0344f70956e170b7ebf2a9c62d91ab12
Suycode05 <suytripathi@gmail.com>
2025-08-21 14:04:35 +0530

    minor changes

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
