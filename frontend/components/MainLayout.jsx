c0ac67bbdec71a2ab976a5121cd9727c123c30b6
Suycode05 <suytripathi@gmail.com>
2025-11-01 22:04:40 +0530

    made minor changes

--- frontend/components/MainLayout.jsx
+++ frontend/components/MainLayout.jsx
@@ -1,12 +1,10 @@
 // MainLayout.jsx
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
-import CustomCursor from "../components/cursor";
 
 export default function MainLayout() {
   return (
     <>
-      <CustomCursor />
       <Navbar />
       <main>
         <Outlet />
