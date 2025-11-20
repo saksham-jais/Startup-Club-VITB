87f4ff981a63aadfad949d928a7406eeba2d4fc5
Saksham Jaiswal <sakshamjais100@gmail.com>
2025-11-20 12:00:11 +0530

    Changement as per discussion

--- frontend/components/StandupRegistration.jsx
+++ frontend/components/StandupRegistration.jsx
@@ -9,7 +9,7 @@ function StandupRegistration({ title = 'Comedy Standup Night' }) {
   const qrCode = "/qr.jpg";
 
   // --- RESPONSIVE BANNER URLS ---
-  const desktopBanner = 'https://res.cloudinary.com/dt83ijcjr/image/upload/v1763544172/event-registrations/standup/zkn1whqjmofjvm3cjgpi.jpg';
+  const desktopBanner = 'https://res.cloudinary.com/dt83ijcjr/image/upload/v1763544172/event-registrations/standup/zkn1whqjmofjvm3cjgppi.jpg';
   const mobileBanner = 'https://res.cloudinary.com/dt83ijcjr/image/upload/v1763544172/event-registrations/standup/zkn1whqjmofjvm3cjgpi.jpg'; // Mobile-specific banner
 
   const [eventData] = useState({ title, bgImage: desktopBanner });
